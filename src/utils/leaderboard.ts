88ad10b7989d72bb59f05c019e782eae887f808d
Arkadiusz Pawlak <arkadiusz.pawlak@goodylabs.com>
2023-05-17 19:10:58 +0200

    changes leaderboard

--- src/utils/leaderboard.ts
+++ src/utils/leaderboard.ts
@@ -1,6 +1,12 @@
 import { APIEmbed, JSONEncodable } from "discord.js";
 import { DATE_FORMAT, DateRange } from "./date-format";
 
+const stndrd = (n: number) => {
+  const s = ["th", "st", "nd", "rd"];
+  const v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+};
+
 export function leaderboardEmbed(
   rank: {
     rankedUserId: string;
@@ -8,40 +14,38 @@ export function leaderboardEmbed(
   }[],
   range: DateRange
 ) {
+  // split the array into 25 chunks
+  const chunks = rank.reduce<NonNullable<APIEmbed["fields"]>[]>(
+    (acc, curr, index) => {
+      const chunkIndex = Math.floor(index / 25);
+
+      if (!acc[chunkIndex]) {
+        acc[chunkIndex] = [];
+      }
+
+      acc[chunkIndex].push({
+        name: `${stndrd(index + 1)} place`,
+        value: `<@${curr.rankedUserId}>\nPoints: ${curr.points}`,
+      });
+
+      return acc;
+    },
+    []
+  );
+
   const titleEmbed: APIEmbed | JSONEncodable<APIEmbed> = {
     title: `🎉🎉 Leaderboard, from ${
       range.from ? range.from.toFormat(DATE_FORMAT) : "ever"
     } - ${range.to ? range.to.toFormat(DATE_FORMAT) : "now"} 🎉🎉`,
     description: `There are ${rank.length} users on the leaderboard, who has at least 1 point!!`,
+    fields: chunks[0],
     color: 0x00ff00,
   };
 
-  const top3Title = ["🥇 1st", "🥈 2nd", "🥉 3rd"];
-  const top3Color = [0xffd700, 0xc0c0c0, 0xcd7f32];
+  const embeds: APIEmbed[] = chunks.slice(1).map((chunk) => ({
+    fields: chunk,
+    color: 0x00ff00,
+  }));
 
-  const top3 = rank
-    .slice(0, 3)
-    .map((user, index): APIEmbed | JSONEncodable<APIEmbed> => ({
-      title: `${top3Title[index]} place`,
-      description: `<@${user.rankedUserId}> has ${user.points} point${
-        user.points > 1 ? "s" : ""
-      }`,
-      color: top3Color[index],
-    }));
-
-  const response = [titleEmbed, ...top3];
-
-  if (rank.length > 3) {
-    const rest: APIEmbed | JSONEncodable<APIEmbed> = {
-      title: "The rest of the leaderboard",
-      fields: rank.slice(0).map((user, index) => ({
-        name: `${index + 4}th place`,
-        value: `<@${user.rankedUserId}>\nPoints: ${user.points}`,
-      })),
-    };
-
-    response.push(rest);
-  }
-
-  return response;
+  return [titleEmbed, ...embeds];
 }
